b851dc1a4a1ed62a1e1c2fba967d37cfe75ddc3d
noise2 <b.g.dariush@gmail.com>
2015-03-29 20:43:59 +0430

    @ TestCases/cppgcTestCaser.hpp: still a test-draft

--- TestCases/cppgcTestCaser.hpp
+++ TestCases/cppgcTestCaser.hpp
@@ -21,6 +21,7 @@ struct derived : public base1, public base2
 {
 };
 
+
 #include <sstream>
 #include <stdlib.h>
 #include <unordered_map>
@@ -34,30 +35,34 @@ namespace cppgc_test {
             int* y;
             double* z;
             {
-                derived obj;
-                gc_ptr<base2> p2 = ref_cast<base2>(&obj);
-                gc_ptr<base1> p1 = ref_cast<base1>(&obj);
+                derived* obj = new derived;
+                derived robj;
+//                gc_ptr<derived> p0(&robj);    // will give memory corruption error on its disposal
+//                gc_ptr<derived> p0 = robj;    // an static_assertion will stop it to work too
+                gc_ptr<derived> p2 = obj;
+                gc_ptr<base1> p1;
+                p1 = p2;
+                gc_ptr<base1> x0 = p1;
                 gc_ptr<int> x = gc_new int(666);
                 gc_ptr<double> d = gc_new double(.666);
+                gc_ptr<int> int1 = gc_new int(10);
+                return;
                 #define _start(x) reinterpret_cast<uintptr_t>(x)
                 #define _end(x) _start(x) + (typeid(x).__is_pointer_p() ? sizeof(*x) : sizeof(x))
-                assert(_start(&obj) <= _start(p1.get()));
-                assert(_end(&obj) >= _end(p1.get()));
-                assert(_start(&obj) <= _start(p2.get()));
-                assert(_end(&obj) >= _end(p2.get()));
+                assert(_start(obj) <= _start(p1.get()));
+                assert(_end(obj) >= _end(p1.get()));
+                assert(_start(obj) <= _start(p2.get()));
+                assert(_end(obj) >= _end(p2.get()));
 
 
-                cout<<&obj<<" "<<sizeof(decltype(obj))<<endl;
+                cout<<obj<<" "<<sizeof(decltype(obj))<<endl;
                 cout<<p1<<" "<<sizeof(*p1.get())<<endl;
                 cout<<p2<<" "<<sizeof(*p1.get())<<endl;
                 cout<<(p1.get())<<" "
                     <<(p2.get())<<" "
                     <<endl;
-                gc_ptr<int> int1 = gc_new int(10);
-                gc_node<int> gn1(&int1);
-                SHOULD_BE(gn1.getAddress(), int1.get());
                 cout<<p1.use_count()<<" "<<p2.use_count()<<endl;
-                p1.reset();
+//                p1.reset();
     //            p2.reset();
                 cout<<p1.use_count()<<" "<<p2.use_count()<<endl;
                 y = x.get();
