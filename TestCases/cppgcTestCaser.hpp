298d025e4090af3cae487305c67af24eec2243a1
noise2 <b.g.dariush@gmail.com>
2015-03-29 06:03:45 +0430

    @ TestCases/cppgcTestCaser.hpp: now i figured how i handle the pointers

--- TestCases/cppgcTestCaser.hpp
+++ TestCases/cppgcTestCaser.hpp
@@ -3,7 +3,6 @@
 
 #include "../../gcpp.hpp"
 #include "../hpp/testCase.hpp"
-#include <typeinfo>
 
 
 struct base1
@@ -31,19 +30,23 @@ namespace cppgc_test {
     public:
         void run(size_t, void**) {
             derived obj;
-            gc_ptr<base1> p1 = ref_cast<base1>(&obj);
-            gc_ptr<base2> p2 = ref_cast<base2>(&obj);
+            gc_ptr<base2> p2 = &obj;
+            gc_ptr<base1> p1 = &obj;
             int* x = new int(666);
             double* d = new double(.666);
+            cout<<&obj<<" "<<sizeof(decltype(obj))<<endl;
             cout<<p1<<" "<<sizeof(decltype(p1))<<endl;
             cout<<p2<<" "<<sizeof(decltype(p2))<<endl;
-            cout<<dynamic_cast<base1*>(p1.get())<<" "
-                <<dynamic_cast<base2*>(p2.get())<<" "
+            cout<<(p1.get())<<" "
+                <<(p2.get())<<" "
                 <<endl;
             gc_ptr<int> int1 = gc_new int(10);
             gc_node<int> gn1(&int1);
             SHOULD_BE(gn1.getAddress(), int1.get());
-            p1.reset();
+            cout<<p1.use_count()<<" "<<p2.use_count()<<endl;
+//            p1.reset();
+//            p2.reset();
+            cout<<p1.use_count()<<" "<<p2.use_count()<<endl;
         }
     };
 }
