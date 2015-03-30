5b17d35a64585132fea028e814f4008ad9a8486a
noise2 <b.g.dariush@gmail.com>
2015-03-30 05:11:26 +0430

    @ CMakeLists.txt: +-DGC_DEBUG

--- TestCases/cppgcTestCaser.hpp
+++ TestCases/cppgcTestCaser.hpp
@@ -7,14 +7,12 @@
 
 struct base1
 {
-   virtual ~base1() { }
    int value1;
 };
 
 struct base2
 {
-   virtual ~base2() { }
-   int value2;
+   int value2, value3;
 };
 
 struct derived : public base1, public base2
@@ -45,23 +43,32 @@ namespace cppgc_test {
                 e("<g<base2>>p02 = <ref<base2>>&robj");
                 gc_ptr<base2> p02  = ref2ptr<base2>(&robj);
 //                gc_ptr<derived> p0_e = robj;                      // an static_assertion will stop it to work too
-                e("<derived>*obj assigned to <g<derived>>p2");
+                e("<g<derived>>p2 = <derived>*obj");
                 gc_ptr<derived> p2 = obj;
-                e("<g<base1>>p1 created");
-                gc_ptr<base1> p1;
+                e("<g<base2>>p1 created");
+                gc_ptr<base2> p1;
                 e("p1 = p2");
                 p1 = p2;
-                e("<g<base1>>x0 = p1");
-                gc_ptr<base1> x0 = p1;
+                e("<g<void>>x0 = p1.static_cast");
+                gc_ptr<void> x0 = p1.get_pure();
+                const void* x01 = x0.get_const();
+                e("const void* x01: "<<x01);
                 e("<g<int>>x created");
                 gc_ptr<int> x = gc_new int(666);
                 e("<g<double>>d created");
                 gc_ptr<double> d = gc_new double(.666);
                 e("<g<double>>int1 created");
                 gc_ptr<int> int1 = gc_new int(10);
-                e("<g<double>>x = <g<int>>d");
+                e("<g<double>>d = <g<int>>x");
                 d = x;
+                int sx = 1;
+                e("<int>sx created: "<<(int*)&sx);
+                e("<g<int>>sxp = <ref<int>>&sx");
+                gc_ptr<int> sxp = ref2ptr<int>(&sx);
             }
+#ifdef GC_DEBUG
+            cout<<gc_void_ptr_t::statistical()<<endl;
+#endif
         }
     };
 }
