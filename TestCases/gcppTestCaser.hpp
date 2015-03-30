4e99d7ef983b2981e55af06e5a47a92b6be47f8f
noise2 <b.g.dariush@gmail.com>
2015-03-30 16:09:37 +0430

    @ TestCases/gcppTestCaser.hpp: +::test_basic() is done

--- TestCases/gcppTestCaser.hpp
+++ TestCases/gcppTestCaser.hpp
@@ -13,13 +13,15 @@
 /**
  * the managed memory size
  */
-#define z       gc::gc_map::get().size()
-#define _(a, b) SHOULD_BE(a, b)
-#define Z(x)    _(z, x)
+#define z           gc::gc_map::get().size()
+#define _(a, b)     SHOULD_BE(a, b)
+#define Z(x)        SHOULD_BE(z, x)
+#define u(a, b)     SHOULD_BE(a.use_count(), b)
+#define uu(a, b)    SHOULD_BE(a.use_count(), b.use_count())
 /**
  * constraint on test: in every test function's entry/exit the gc_map.size() must be equal to zero
  */
-#define enter_test  cout<<"  [.]"<<__func__; IS_ZERO(z)
+#define enter_test  cout<<"  [.]"<<__func__<<"()"; IS_ZERO(z)
 #define exit_test   cout<<"\r  [\u221A]"<<endl; IS_ZERO(z); return true
 namespace cppgc_test {
     using namespace gc;
@@ -30,32 +32,43 @@ namespace cppgc_test {
         void run(size_t, void**) {
             cout<<endl;
             BESURE(this->test_basic());
+            BESURE(this->test_inherit());
         }
     protected:
+        template<typename T>
+        using p = gc_ptr<T>;
         bool test_basic() {
             enter_test;
-            gc_ptr<int> x;
+            p<int> x;
             // empty gc_ptr creation should not modify gc_map(the map)!
             Z(0);
             {
                 // assigning a stack var
                 x = 1;
                 // should not modify the map
-                Z(0), _(x.use_count(), 1);
+                Z(0), u(x, 1);
                 // create a new instance of x, test and access it's value by gc_ptr's operator*()
                 SHOULD_NOT_THROW(x = gcnew int(*x + 1));
                 // the map should be in creased by one
                 Z(1);
                 // the value assignment
-                _(*x, 2), _(x.use_count(), 1);
+                _(*x, 2), u(x, 1);
                 // make a ref. copy
-                gc_ptr<int> y = &*x;
+                p<int> y = &*x;
                 // since we used pointing to a same point, the use count should be 2
-                IS_EQUAL(x.use_count(), y.use_count()), _(x.use_count(), 2);
+                uu(x, y), u(x, 2);
+                // just a memory copy on stack, no re assignment happened
+                p<int> k = y;
+                // same senario, but still the use count should be 2
+                uu(x, y), uu(x, k), u(k, 2);
             }
             Z(1);
             // dispose x
             x.reset();
+            exit_test;
+        }
+        bool test_inherit() {
+            enter_test;
             exit_test;
         }
     };
