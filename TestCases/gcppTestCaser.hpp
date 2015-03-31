fc3c32c094225d7b58b41ef021b4ba9978a7ddf5
noise2 <b.g.dariush@gmail.com>
2015-03-31 12:30:51 +0430

    @ TestCases/gcppTestCaser.hpp: +::test_cast()

--- TestCases/gcppTestCaser.hpp
+++ TestCases/gcppTestCaser.hpp
@@ -18,6 +18,7 @@
 #define Z(x)        SHOULD_BE(z, x)
 #define u(a, b)     SHOULD_BE(a.use_count(), b)
 #define uu(a, b)    SHOULD_BE(a.use_count(), b.use_count())
+#define ptoi(p)                      reinterpret_cast<std::intptr_t>(p)
 /**
  * constraint on test: in every test function's entry/exit the gc_map.size() must be equal to zero
  */
@@ -32,7 +33,7 @@ namespace cppgc_test {
         void run(size_t, void**) {
             cout<<endl;
             BESURE(this->test_basic());
-            BESURE(this->test_value());
+            BESURE(this->test_cast());
         }
     protected:
         template<typename T>
@@ -46,21 +47,21 @@ namespace cppgc_test {
                 // assigning a stack var
                 x = 1;
                 // should not modify the map
-                Z(0), u(x, 1);
+                Z(0), u(x, 1), IS_TRUE(x.is_on_stack());
                 // create a new instance of x, test and access it's value by gc_ptr's operator*()
                 SHOULD_NOT_THROW(x = gcnew int(*x + 1));
                 // the map should be in creased by one
                 Z(1);
                 // the value assignment
-                _(*x, 2), u(x, 1);
+                _(*x, 2), u(x, 1), IS_FALSE(x.is_on_stack());
                 // make a ref. copy
                 p<int> y = &*x;
                 // since we used pointing to a same point, the use count should be 2
-                uu(x, y), u(x, 2);
+                uu(x, y), u(x, 2), IS_FALSE(y.is_on_stack());
                 // just a memory copy on stack, no re assignment happened
                 p<int> k = y;
                 // same senario, but still the use count should be 2
-                uu(x, y), uu(x, k), u(k, 2);
+                uu(x, y), uu(x, k), u(k, 2), IS_FALSE(k.is_on_stack());;
                 // try to change the value throw operator*()
                 NOT_EQUAL(*x, 666), *x = 666, IS_EQUAL(*x, 666);
             }
@@ -69,14 +70,30 @@ namespace cppgc_test {
             x.reset();
             exit_test;
         }
-        bool test_value() {
+        bool test_cast() {
             enter_test;
-            auto a = 1.1;
-            p<int> _int = (int)a;
-            _(*_int, 1), IS(*_int, int);
-            p<double> _double = gc_cast<double>(_int);
-            _(*_double, 1);
-            IS(*_double, double);
+            {
+                // convertion of double to int, must be done by client code
+                p<int> _int = (int)1.1;
+                // validate the values
+                _(*_int, 1), IS(*_int, int), IS_TRUE(_int.is_on_stack());
+                p<base1> _base1 = new base1; _base1->bval1 = 1;
+                IS(*_base1, base1), _(_base1->bval1, 1);
+                p<base2> _base2 = new base2; _base2->bval2 = 2;
+                IS(*_base2, base2), _(_base2->bval2, 2);
+                p<derived1> _d1 = new derived1; _d1->bval1 = 11, _d1->dval1 = 1;
+                _(_d1->bval1, 11), _(_d1->dval1, 1);
+                p<derived12> _d12 = new derived12; _d12->bval1 = 121; _d12->bval2 = 122;
+                _(_d12->bval1, 121), _(_d12->bval2, 122);
+                Z(4);
+                _base1 = _d1;
+                // last pointer dies, increments the _d1's ref count
+                Z(3), _(gc_map::get().at(_d1.get_pure()), 2);
+                _(_base1.get_pure(), _d1.get_pure());
+                _base1->bval1++;
+                _(_base1->bval1, _d1->bval1);
+
+            }
             exit_test;
         }
     };
