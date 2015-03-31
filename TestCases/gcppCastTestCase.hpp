14a6c56319a4c67e5658d6bd03e162115cd48cf3
noise2 <b.g.dariush@gmail.com>
2015-03-31 17:52:03 +0430

    @ TestCases/gcppCastTestCase.hpp: defiantly there is a some thing wrong with how i use can_dynamic_cast()

--- TestCases/gcppCastTestCase.hpp
+++ TestCases/gcppCastTestCase.hpp
@@ -5,19 +5,36 @@
 #include <vector>
 #include <cxxabi.h>
 #include <type_traits>
-#include "../hpp/teststrap.hpp"
 #include "../hpp/testCase.hpp"
+#include "../hpp/teststrap.hpp"
 //#include "../../../gcpp/gcpp.1/gc_cast.hpp"
 
-/*#   define can_cast(FROM, to)           std::is_convertible<FROM, to>()::value
-//#   define can_cast_ptr(FROM, to)       can_cast(FROM*, to*)
-//#   define can_dynamic_cast(FROM, to)   false;
-//                                        can_cast(FROM, to) && \
-//                                        !std::is_same<FROM, to>()::value && \
-//                                        !std::is_const<FROM>()::value && \
-//                                        std::is_class<TO>()::value && \
-//                                        std::is_base_of<TO, FROM>()::value
-//#   define can_static_cast(FROM, to)    can_cast(FROM, to)*/
+#   define can_cast(FROM, TO)           std::is_convertible<FROM, TO>::value
+#   define can_cast_ptr(FROM, TO)       can_cast(FROM*, to*)
+#   define can_dynamic_cast(FROM, TO)   \
+                                        std::is_convertible<FROM, TO>::value && \
+                                        !std::is_same<FROM, TO>::value && \
+                                        !std::is_const<FROM>::value /*&& \
+                                        std::is_class<TO>::value && \
+                                        std::is_base_of<TO, FROM>::value*/
+#   define can_static_cast(FROM, TO)    can_cast(FROM, TO)
+
+
+struct base1 { int bval1; double* bp1; };
+struct base2 { int bval2; double* bp2; };
+struct base3 { int bval3; double* bp3; };
+
+struct derived1 : public base1 { int dval1; double* dp1; };
+struct derived2 : public base2 { int dval2; double* dp2; };
+struct derived3 : public base3 { int dval3; double* dp3; };
+
+struct derived12 : public base1, public base2 { int dval12; double* dp1; };
+struct derived23 : public base2, public base3 { int dval23; double* dp2; };
+struct derived31 : public base3, public base1 { int dval31; double* dp3; };
+
+struct hderived123 : public derived12, public base3 { int hdval123; double* hdp1; };
+struct hderived231 : public derived23, public base1 { int hdval231; double* hdp2; };
+struct hderived312 : public derived31, public base2 { int hdval312; double* hdp3; };
 
 bool fetch_cond(std::vector<bool>& cond) {
     bool res = true;
@@ -25,31 +42,21 @@ bool fetch_cond(std::vector<bool>& cond) {
     return res;
 }
 
-template<typename FROM, typename to>
-bool can_cast(bool type = 1, typename std::enable_if<std::is_convertible<FROM, to>::value>::type* =0) { return !type; }
-template<typename FROM, typename to>
-bool can_static_cast(bool type = 1, typename std::enable_if<
-    std::is_convertible<FROM, to>::value
->::type* = 0) { return !type; }
-template<typename FROM, typename to>
-bool can_dynamic_cast(bool type = 1, typename std::enable_if<
-    std::is_convertible<FROM, to>::value
->::type* = 0) { return !type; }
-
 namespace cppgc_test {
     class gcppCastTestCase : public CPP_TESTER::testCase {
         void run(size_t, void**) {
-#define fail(from, to, e) {cout<<endl<<"@line "<<__LINE__<<": "<<std::string(std::string(e? "" : "didn't")+"expect to cast from `"+type_name<from>()+"` to `"+type_name<to>()+"`; but "+std::string(e?"didn't":"did")+"!"); exit(0);}
-#define c(from, to) if(!can_cast<from, to>())         fail(from, to, 1)
-#define s(from, to) if(!can_static_cast<from, to>())  fail(from, to, 1)
-#define d(from, to) if(!can_dynamic_cast<from, to>()) fail(from, to, 1)
-#define nc(from, to) if(can_cast<from, to>(0))         fail(from, to, 0)
-#define ns(from, to) if(can_static_cast<from, to>(0))  fail(from, to, 0)
-#define nd(from, to) if(can_dynamic_cast<from, to>(0)) fail(from, to, 0)
+#define fail(from, to, e) {cout<<endl<<"@line "<<__LINE__<<": "<<std::string(std::string(e? "" : "didn't")+"expect to cast from `"+type_name<from>()+"` to `"+type_name<to>()+"`; but "+std::string(e?"didn't":"did")+"!")<<endl; terminate();}
+#define c(from, to)  static_assert(can_cast(from, to), "expecting to cast, but couldn't!")
+#define s(from, to)  static_assert(can_static_cast(from, to), "expecting static cast, but couldn't!")
+#define d(from, to)  static_assert(can_dynamic_cast(from, to), "expecting dynamic cast, but couldn't!")
+#define nc(from, to) static_assert(!can_cast(from, to), "didn't expecting to cast, but could!")
+#define ns(from, to) static_assert(!can_static_cast(from, to), "didn't expecting static cast, but could!")
+#define nd(from, to) static_assert(!can_dynamic_cast(from, to), "didn't expecting dynamic cast, but could!")
             s(int, int);
             s(double, int);
-            nd(double, int);
-            nd(int, double);
+            nd(int, int); // not-same check
+//            nd(int, double);
+//            ns(base1, int);
         }
     protected:
         template<typename T>
